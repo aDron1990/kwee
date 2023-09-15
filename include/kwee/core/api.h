96246d4f4055d2a5ab12d925b648917cea542d44
Andrey <andreysheveliov1990@gmail.com>
2023-09-16 00:40:18 +0300

    modified:   include/kwee/core/api.h

--- include/kwee/core/api.h
+++ include/kwee/core/api.h
@@ -1,16 +1,11 @@
 #pragma once
 
 #ifdef _WIN32
-
-	
 	#ifdef KWEE_BUILD
 		#define KWEEAPI __declspec(dllexport)
 	#else
 		#define KWEEAPI __declspec(dllimport)
 	#endif
-
 #else
-
-	#error Unknown os!
-
+	#define KWEEAPI
 #endif
