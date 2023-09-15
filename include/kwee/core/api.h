10b520e84e0fb6bb02772bbbde50763a301d4b20
Andrey <andreysheveliov1990@gmail.com>
2023-09-15 21:18:33 +0300

    On branch cmake_port

---
+++ include/kwee/core/api.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef _WIN32
+
+	
+	#ifdef KWEE_BUILD
+		#define KWEEAPI __declspec(dllexport)
+	#else
+		#define KWEEAPI __declspec(dllimport)
+	#endif
+
+#else
+
+	#error Unknown os!
+
+#endif
